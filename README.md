592f7182fb78b7777d81776598cf6d92edaf3f3b
Jan <info@phoca.cz>
2017-10-26 15:13:52 +0200

    3.0.3

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# PhocaOpenGraphSystemPlugin
-Phoca Open Graph System Plugin is Joomla! CMS extension. Plugin which adds open graph information into website (except article sites - see Phoca Open Graph Content Plugin).
-
+Phoca Open Graph System Plugin is Joomla! CMS extension. Plugin which adds open graph information and Tweets with Cards information into website.
--- phocaopengraph.php
+++ phocaopengraph.php
@@ -10,67 +10,196 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 jimport( 'joomla.plugin.plugin' );
 
-class plgSystemPhocaOpenGraphHelper
+
+class plgSystemPhocaOpenGraph extends JPlugin
 {
-	public static function renderTag($name, $value, $type = 1) {
+	
+	public $twitterEnable 	= 0;
+	
+	public function __construct(& $subject, $config) {
+		parent::__construct($subject, $config);
+	}
+	
+	
+	public function renderTag($name, $value, $type = 1) {
 		$document 	= JFactory::getDocument();
+		$docType	= $document->getType();
+		if ($docType == 'pdf' || $docType == 'raw' || $docType == 'json') {
+			return;
+		}
+		
+		// OG
 		if ($type == 1) {
 			$document->setMetadata(htmlspecialchars($name), htmlspecialchars($value));
 		} else {
 			$document->addCustomTag('<meta property="'.htmlspecialchars($name).'" content="' . htmlspecialchars($value) . '" />');
 		}
-	}
-	
-}
-
-class plgSystemPhocaOpenGraph extends JPlugin
-{
-	public function __construct(& $subject, $config) {
-		parent::__construct($subject, $config);
+		
+		// Tweet with cards
+		if ($this->twitterEnable == 1) {
+			if ($name == 'og:title') {
+				$document->setMetadata('twitter:title', htmlspecialchars($value));
+			}
+			if ($name == 'og:description') {
+				$document->setMetadata('twitter:description', htmlspecialchars($value));
+			}
+			if ($name == 'og:image') {
+				$document->setMetadata('twitter:image', htmlspecialchars($value));
+			}
+		}
 	}
 	
 	/*function onBeforeRender() {
 		$app 	= JFactory::getApplication();
-		$option	= JRequest::getCmd('option');
-		//$view	= JRequest::getCmd('view');
+		
 		
 		if ($option == 'com_phocainclude') {
 			$document 	= JFactory::getDocument();
 			$type		= $this->params->get('render_type', 1);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:site_name', 'Phoca', $type);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:title', $document->title, $type);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:description', $document->description, $type);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:url', $document->base, $type);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:type', 'website', $type);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:image', 'http://www.phoca.cz/phoca.png', $type);	
+			$this->renderTag('og:site_name', 'Phoca', $type);
+			$this->renderTag('og:title', $document->title, $type);
+			$this->renderTag('og:description', $document->description, $type);
+			$this->renderTag('og:url', $document->base, $type);
+			$this->renderTag('og:type', 'website', $type);
+			$this->renderTag('og:image', 'http://www.phoca.cz/phoca.png', $type);	
 		}
 	}*/
 	
 	function onBeforeRender() {
 		$app 	= JFactory::getApplication();
-		$option	= JRequest::getCmd('option');
-		//$view	= JRequest::getCmd('view');
+		$option	= $app->input->get('option');
+		$view	= $app->input->get('view');
+		$format = $app->input->get('format');
 		
-		$format = JRequest::getWord('format');
-		if ($format == 'feed' || $format == 'pdf' || $format == 'json') {
+		if ($format == 'feed' || $format == 'pdf' || $format == 'json' || $format == 'raw') {
 			return true;
 		}
 		
+		
 		if ($app->getName() != 'site') { return;}
+		
+		
+		// Allow to display in article
+		$allowed		= 0;
+		$articleIds 		= $this->params->get('enable_article', '');
+		
+		if ($option == 'com_content' && $view == 'categories') {
+			$allowed		= 1;
+		} else if ($option == 'com_content') {
+			if ($articleIds != '') {
+				$articleIdsA =  explode(',', $articleIds);
+				if (!empty($articleIdsA)) {
+					$articleId	= $app->input->get('id', 0, 'int');
+					foreach ($articleIdsA as $k => $v) {
+						if ($option == 'com_content' && (int)$articleId > 0 &&(int)$articleId == (int)$v) {
+							//$articleAllowed = (int)$articleId;
+							$allowed = (int)$articleId;
+							break;
+						}
+					}
+				}
+			}
+		}else if ($option != 'com_content') {
+			$allowed		= 1;
+		}
 		
 		//com_phocadownload
 		//com_phocadocumentation
 		//com_phocainclude
-		if ($option != 'com_content') {
-			$document 	= JFactory::getDocument();
+		
+		if ($allowed > 0) {
 			
-			$type		= $this->params->get('render_type', 1);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:site_name', 'Phoca', $type);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:title', $document->title, $type);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:description', $document->description, $type);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:url', $document->base, $type);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:type', 'website', $type);
-			plgSystemPhocaOpenGraphHelper::renderTag('og:image', 'http://www.phoca.cz/phoca.png', $type);	
+			$document 				= JFactory::getDocument();
+			$config 				= JFactory::getConfig();
+			$type					= $this->params->get('render_type', 1);
+			$this->twitterEnable 	= $this->params->get('twitter_enable', 0);
+			$twitterCard 			= $this->params->get('twitter_card', 'summary_large_image');
+			
+			if ($this->twitterEnable == 1) {
+				$this->renderTag('twitter:card', htmlspecialchars($twitterCard), 1);
+				
+				if ($this->params->get('twitter_site', '') != '') {
+					$this->renderTag('twitter:site', $this->params->get('twitter_site', ''), 1);
+				}
+				
+				if ($this->params->get('twitter_site', '') != '') {
+					$this->renderTag('twitter:creator', $this->params->get('twitter_creator', ''), 1);
+				}
+			}
+			
+			
+			// Site Name
+			if ($this->params->get('site_name', '') != '') {
+				$this->renderTag('og:site_name', $this->params->get('site_name', ''), $type);
+			} else {
+				$this->renderTag('og:site_name', $config->get('sitename'), $type);
+			}
+			$this->renderTag('og:title', $document->title, $type);
+			$this->renderTag('og:description', $document->description, $type);
+			$this->renderTag('og:url', $document->base, $type);
+			$this->renderTag('og:type', 'website', $type);
+			
+			
+			// Try to find image in content
+			$imgSet = 0;
+			if ($this->params->get('find_image_content') == 1) {
+				
+				$buffer = $document->getBuffer();
+				$docB 	= '';
+				if (isset($buffer['component']) && is_array($buffer)) {
+					foreach ($buffer['component'] as $v) {
+						if (is_array($v)) {
+							foreach($v as $v2) {
+								$docB .= (string)$v2;
+							}
+						} else {
+							$docB .= (string)$v;
+						}
+					}
+				} else {
+					$docB .=	(string)$buffer;
+				}
+				
+				preg_match('/< *img[^>]*src *= *["\']?([^"\']*)/i', $docB, $src);
+				if (isset($src[1]) && $src[1] != '') {
+					
+					$absU = 0;
+					// Test if this link is absolute http:// then do not change it
+					$pos1 			= strpos($src[1], 'http://');
+					if ($pos1 === false) {
+					} else {
+						$absU = 1;
+					}
+					
+					// Test if this link is absolute https:// then do not change it
+					$pos2 			= strpos($src[1], 'https://');
+					if ($pos2 === false) {
+					} else {
+						$absU = 1;
+					}
+					
+					if ($absU == 1) {
+						$linkImg = $src[1];
+					} else {
+						$linkImg = JURI::base(false).$src[1];
+						if ($src[1][0] == '/') {
+							$myURI = new \Joomla\Uri\Uri(JURI::base(false));
+							$myURI->setPath($src[1]);
+							$linkImg = $myURI->toString();
+						} else {
+							$linkImg = JURI::base(false).$src[1];
+						}
+					}
+					
+					
+					$this->renderTag('og:image', $linkImg, $type);
+					$imgSet = 1;
+				}
+			}
+			
+			if ($this->params->get('image') != '' && $imgSet == 0) {
+				$this->renderTag('og:image', JURI::base(false).$this->params->get('image'), $type);
+			}
 		}
 	}
 }
